dd21e4c136d64782b147004149e454d194a924d9
BubblyNae <nathan.jolobelawson@rocketmail.com>
2025-12-28 03:20:00 +0000

    Added Install scripts for nvim, hyprland and dotfiles
    The install_dotfiles script is from typecraft's "You Installed Omarchy,
    Now What?" video for installing dotfiles using stow

---
+++ InstallScripts/install_hypr.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pacman -S --noconfirm --needed hyprland
+yay -S --noconfirm --needed hyprlock
+yay -S --noconfirm --needed hyprpaper
--- InstallScripts/master_installation.sh
+++ InstallScripts/master_installation.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 . ./install_stow.sh
+. ./install_nvim.sh
+. ./install_hypr.sh
+. ./install_dotfiles.sh
