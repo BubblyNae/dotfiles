7d034e3227ab1ed3a7b74c010b482799079cf7b4
BubblyNae <nathan.jolobelawson@rocketmail.com>
2026-03-02 21:38:28 +0000

    Added hyprcursor to the list of hypr packages in the hypr install script

--- InstallScripts/install_hypr.sh
+++ InstallScripts/install_hypr.sh
@@ -4,3 +4,4 @@ pacman -S --noconfirm --needed hyprland
 yay -S --noconfirm --needed hyprlock
 yay -S --noconfirm --needed hyprpaper
 yay -S --noconfirm --needed hyprmixer
+yay -S --noconfirm --needed hyprcursor
